b61bf8e96f80d9a730bb5cce6bf8b93cb643539c
iSebasC <sebas.cabrera.456@gmail.com>
2025-03-21 15:55:28 -0500

    bd

--- config/db.js
+++ config/db.js
@@ -1,17 +1,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,  // Número máximo de conexiones simultáneas
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Conexión exitosa con la base de datos MySQL");
+// Conexión de prueba
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ Error al conectar con la base de datos:", err.code);
+  } else {
+    console.log("✅ Conexión exitosa con la base de datos MySQL");
+    connection.release(); // Libera la conexión cuando termine
+  }
 });
 
-module.exports = db;
+module.exports = pool;
